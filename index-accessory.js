cff7954171960e59487ff97fc4b465facafacbca
Christian Wegerhoff <wegerhoff@mediaworx.com>
2018-01-25 16:35:12 +0100

    Changed to platform

---
+++ index-accessory.js
@@ -0,0 +1,55 @@
+var request = require('request');
+var url = require('url');
+
+var Service, Characteristic;
+ 
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("empirbus-switch-plugin", "EmpirBusSwitch", EmpirBusSwitch);
+};
+
+ 
+function EmpirBusSwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+ 
+EmpirBusSwitch.prototype = {
+ 
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        url: me.getUrl,
+        method: 'GET',
+    }, 
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      me.log ('Switch Status: ' + body.status);
+      return next(null, body.status);
+    });
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
