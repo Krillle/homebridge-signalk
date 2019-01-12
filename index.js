54b6a17295d4f011b9c03bf39acd224cbdba4212
Christian Wegerhoff <wegerhoff@mediaworx.com>
2019-01-12 10:48:57 +0100

    fiy: Error messages, response 202

--- index.js
+++ index.js
@@ -464,10 +464,10 @@ SignalKPlatform.prototype.addDimmerServices = function(accessory) {
     // console.log(stateBefore);
 
     // Off/On/Off/Restore cycle
-    platform.setOnOff(accessory.context.identifier, false, ()=> {console.log('FIXME: Device unreachable');});
-    setTimeout(()=>{platform.setOnOff(accessory.context.identifier, true, ()=> {platform.log('FIXME: Device unreachable');}) // FIXME: Device unreachable
+    platform.setOnOff(accessory.context.identifier, false, (E, M)=> {platform.log('Device unreachable:', E, M);});
+    setTimeout(()=>{platform.setOnOff(accessory.context.identifier, true, (E, M)=> {platform.log('Device unreachable:', E, M);})
                    }, 250);
-    setTimeout(()=>{platform.setOnOff(accessory.context.identifier, false, ()=> {platform.log('FIXME: Device unreachable');}) // FIXME: Device unreachable
+    setTimeout(()=>{platform.setOnOff(accessory.context.identifier, false, (E, M)=> {platform.log('Device unreachable:', E, M);})
                    }, 750);
     // FIXME: Restore original state of device before cycle
     //  setTimeout(()=>{platform.setOnOff(identifier, stateBefore)}, 1000);
@@ -484,7 +484,7 @@ SignalKPlatform.prototype.addDimmerServices = function(accessory) {
   .on('get', this.getOnOff.bind(this, dataPath))
   .on('set', function(value, callback) {
     platform.log(`Set dimmer ${accessory.displayName}.state to ${value}`)
-    platform.setOnOff(accessory.context.identifier, value, (E, M)=> {platform.log('Device unreachable:');}) // FIXME: Device unreachable
+    platform.setOnOff(accessory.context.identifier, value, (E, M)=> {platform.log('Device unreachable:', E, M);})
     callback();
   })
 
@@ -1144,10 +1144,10 @@ SignalKPlatform.prototype.setValue = function(device, context, value, cb) {
               cb(error, null)     // FIXME: Error is not used
             } else if ( response.statusCode == 401 ) {
               this.log(`response: ${response.statusCode} ${response.request.method} ${response.request.uri.path}`)
-              cb(new Error(`UNAUTHORIZED missing, bad or expired token ${response.statusCode}`), null)     // FIXME: Error is not used
-            } else if ( response.statusCode != 200 ) {   // FIXME: Venus GX response is 202 SUCCESS
+              cb(null, `UNAUTHORIZED missing, bad or expired Signal K security token ${response.statusCode}`)
+            } else if ( response.statusCode != 200 && response.statusCode != 202 ) {   // EmpirBus resonse is 200 OK, Venus GX response is 202 SUCCESS
               this.log(`response: ${response.statusCode} ${response.request.method} ${response.request.uri.path}`)
-              cb(new Error(`invalid response ${response.statusCode}`), null)     // FIXME: Error is not used
+              cb(new Error(`invalid response ${response.statusCode}`), null)
             } else {
               httpLog('Success.')
               cb(null, null)
