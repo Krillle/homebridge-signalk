f1cda61561e9985eb0fd946cf4e08444fa1bf769
Christian Wegerhoff <Krillle@users.noreply.github.com>
2022-11-10 13:56:48 +0700

    FIX: storagePath() / this.kfs

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ function SignalKPlatform(log, config, api) {
       this.api = api;
       
       // Initialie key value store 
-      platform.log("Create keyFileStorage at", this.api.user.storagePath);      
+      platform.log("Create keyFileStorage at", this.api.user.storagePath() );      
       this.kfs = keyFileStorage.default('/var/lib/homebridge', true);
 //      this.kfs = keyFileStorage.default(this.api.user.storagePath, true);
 
@@ -305,7 +305,7 @@ function SignalKPlatform(log, config, api) {
         setInterval(platform.autodetectNewAccessories.bind(this), platform.autodetectNewAccessoriesInterval);
 
         // Periodically check status of Signal K access request
-        if ((this.config.accessRequest || true ) && kfs['requestStatus'] != 'APPROVED') {
+        if ((this.config.accessRequest || true ) && this.kfs['requestStatus'] != 'APPROVED') {
           setTimeout(platform.accessRequest.bind(this), platform.accessRequestInterval);
           setInterval(platform.accessRequest.bind(this), platform.accessRequestInterval);
         }
